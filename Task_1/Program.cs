9a3af19a454c3a42a794fc3f55ec77e973321a5e
XFX <sah0329@mail.ru>
2022-07-27 23:33:49 +0500

    com

---
+++ Task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+    Задача 64: 
+    Задайте значения M и N. Напишите программу, 
+которая выведет все натуральные числа в промежутке от M до N.
+*/
+
+void f(int n, int m)
+{
+    if (n != (m - 1))
+    {
+        f(n - 1, m);
+        Console.Write(n + "\t");
+    }
+}
+Console.Write("Задайте значения M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте значения N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+f(n,m);
---
+++ Task_2/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+    Задача 66: 
+    Задайте значения M и N. Напишите программу, 
+которая найдёт сумму натуральных элементов в промежутке от N до M.
+*/
+
+int f(int n, int m)
+{
+    if (n > m)
+        return 0;
+    return f(n + 1, m) + n;
+}
+Console.Write("Задайте значения N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте значения M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write(f(n, m));
